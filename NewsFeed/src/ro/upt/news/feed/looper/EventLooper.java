5443fd49cd544ef3d476fa87dd41ad3f934b88bb
Varga Roland <varga.roland92@yahoo.com>
2015-01-03 12:48:35 +0200

    first commit

--- NewsFeed/src/ro/upt/news/feed/looper/EventLooper.java
+++ NewsFeed/src/ro/upt/news/feed/looper/EventLooper.java
@@ -3,5 +3,6 @@ package ro.upt.news.feed.looper;
 public class EventLooper {
 	public static void main(String[] args) {
 		System.out.println("Hello EventLooper!");
+		System.out.println("Hello again!");
 	}
 }
